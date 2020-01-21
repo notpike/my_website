74a4a76a560e40542efa2fb9ada02847ebdb7c7a
notpike <notpike@horsefucker.org>
2020-01-21 15:39:13 -0500

    Added hackers.txt

--- public/js/logic.js
+++ public/js/logic.js
@@ -8,7 +8,8 @@
 //Flashing lights and cold hard steel!
 //https://www.oreilly.com/library/view/css-cookbook/0596005768/ch03s03.html
 function blink(delay) {
-    var target = document.body.getElementsByTagName('SPAN');
+
+    var target = document.body.getElementsByTagName('span');
     for(i = 0; i< target.length; i++) {
         target[i].style.visibility = target[i].style.visibility == 
         'hidden' ? 'visible' : 'hidden';
