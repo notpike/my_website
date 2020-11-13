281a428c14012b19f5c126749100aa06c365103b
Jake s <jacob@codeintel.com>
2020-11-12 22:37:40 -0500

    Test

--- apps/webHook.js
+++ apps/webHook.js
@@ -7,19 +7,32 @@
 
 /* ------------ REQUIREMENTS ------------ */
 require('dotenv').config()
+const Logger = require('./apps/logger');
 const crypto = require('crypto');
 const exec = require('child_process').exec;
 
  class WebHook {
 
     webHook(req) {
+
+        const logger = new Logger();
+        logger.on('message', (data) => console.log("Called Listener: ", data));
+
         const repo = "/var/www/my_website";
         const secret = process.env.WEBHOOK_PASS;
         
         req.on('data', function (chunk) {
             let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
             if (req.headers['x-hub-signature'] == sig) {
-                exec('cd ' + repo + ' && git pull');
+                exec('cd ' + repo + ' && git pull', (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`exec error: ${error}`);
+                        return;
+                    }
+                    console.log(`stdout: ${stdout}`);
+                    console.error(`stderr: ${stderr}`);
+                });
+                
                 exec('pm2 restart all');
             }
         });
