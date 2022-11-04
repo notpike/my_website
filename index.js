2ac0d03379b95ee3f5a8475c4f760f1809a7f890
notpike <e1000basetx@gmail.com>
2022-11-04 15:34:16 -0400

    Fixed port number

--- index.js
+++ index.js
@@ -159,5 +159,5 @@ const server = http.createServer((req,res) => {
 
 
 /* ---------- MAIN SERVER START ---------- */
-const PORT = process.env.PORT || 5000; // envirement var or 5050
+const PORT = process.env.PORT || 5001; // envirement var or 5050
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
